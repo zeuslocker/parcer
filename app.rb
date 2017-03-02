8336eb199de9dd05f772f1c7f47781f80a814653
zeuslocker <lapan40@ukr.net>
2017-03-02 09:11:36 +0200

    add category index

--- app.rb
+++ app.rb
@@ -9,30 +9,26 @@ categories << open('http://www.citrus.ua/shop/goods/tabletpc/667/')
 categories << open('http://www.citrus.ua/shop/goods/smart-watches/2070/')
 categories << open('http://www.citrus.ua/shop/goods/notebooks/669/')
 categories << open('http://www.citrus.ua/shop/goods/monoblocks/1427/')
-
+productsArr = []
 documents = categories.map do |category|
   Nokogiri::HTML(category)
 end
 products = []
-documents.each do |doc|
+documents.each_with_index do |doc, index|
   doc.css('.module_item .el_pic table tr td a').each do |photo|
-    products << 'http://www.citrus.ua'+photo.attributes['href'].value
-  end
-end
-productsArr = []
-products.each do |product|
-product =  Nokogiri::HTML(open(product))
-images = []
-  product.css('.bb_big_photo_box2 .owl-carousel .item').each do |image|
-    str = image.attributes['style'].value
-  images << 'http://www.citrus.ua'+str[str.index('/upload'), str.index(')')-str.index('/upload')]
-  end
-  price = nil
-  if product.at_css('.price').text.to_f > product.at_css('.product_price').text.to_f
-    price = product.at_css('.price').text.to_f
-  else
-    price = product.at_css('.product_price').text.to_f
-  end
+    product = 'http://www.citrus.ua'+photo.attributes['href'].value
+    product =  Nokogiri::HTML(open(product))
+    images = []
+    product.css('.bb_big_photo_box2 .owl-carousel .item').each do |image|
+      str = image.attributes['style'].value
+      images << 'http://www.citrus.ua'+str[str.index('/upload'), str.index(')')-str.index('/upload')]
+    end
+    price = nil
+    if product.at_css('.price').text.to_f > product.at_css('.product_price').text.to_f
+      price = product.at_css('.price').text.to_f
+    else
+      price = product.at_css('.product_price').text.to_f
+    end
   description = product.at_css('p[20]')&.text || 'ttttttrrruuueeee'
   show_case = product.at_css('.tabs_content').to_html
   productsArr.push(
@@ -44,5 +40,6 @@ images = []
   images_links: images,
   show_case: show_case
   )
-  p roductsArr
+  pp productsArr
+end
 end
